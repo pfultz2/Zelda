6b6ec5ae02503cf2df3705c938d680c528385f3f
paul <paul@paul-TPOWER-X79>
2012-10-13 22:32:41 -0500

    Add poly and adaptor base

---
+++ zelda/function/adaptor.h
@@ -0,0 +1,45 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    adaptor.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_FUNCTION_ADAPTOR_H
+#define ZELDA_GUARD_FUNCTION_ADAPTOR_H
+
+#include <zelda/pp.h>
+#include <zelda/requires.h>
+#include <boost/type_traits.hpp>
+
+namespace zelda {
+
+//Optimization for stateless functions
+template<class F, class Enable = void>
+struct function_adaptor_base
+{
+    F f;
+    function_adaptor_base() {};
+    function_adaptor_base(F f) : f(f) {};
+
+    F get_function() const
+    {
+        return f;
+    }
+};
+
+template<class F>
+struct function_adaptor_base<F, ZELDA_CLASS_REQUIRES(boost::is_empty<F>)>
+{
+    function_adaptor_base() {};
+    function_adaptor_base(F) {};
+
+    F get_function() const
+    {
+        return F();
+    }
+};
+
+}
+
+#endif
