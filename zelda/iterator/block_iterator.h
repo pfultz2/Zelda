e92c4bfa3dd26d9e638eb898699a5872af35ee07
paul <paul@paul-TPOWER-X79>
2012-11-08 15:37:25 -0600

    Add initial iterators

---
+++ zelda/iterator/block_iterator.h
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    block_iterator.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ITERATOR_BLOCK_ITERATOR_H
+#define ZELDA_GUARD_ITERATOR_BLOCK_ITERATOR_H
+
+namespace zelda { namespace iterator {
+
+class block_iterator
+{
+
+};
+
+}}
+
+#endif
---
+++ zelda/iterator/unfold_iterator.h
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    unfold_iterator.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ITERATOR_UNFOLD_ITERATOR_H
+#define ZELDA_GUARD_ITERATOR_UNFOLD_ITERATOR_H
+
+namespace zelda { namespace iterator {
+
+class unfold_iterator
+{
+
+};
+
+}}
+
+#endif
