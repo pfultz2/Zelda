554586f3579e143ca0ba5a8afa55411465ceee78
paul <paul@paul-TPOWER-X79>
2012-11-14 15:57:38 -0600

    Added directory_range

--- zelda/range/directory_range.h
+++ zelda/range/directory_range.h
@@ -8,13 +8,25 @@
 #ifndef ZELDA_GUARD_RANGE_DIRECTORY_RANGE_H
 #define ZELDA_GUARD_RANGE_DIRECTORY_RANGE_H
 
-namespace zelda { namespace range {
+#include <boost/filesystem/operations.hpp>
+#include <zelda/range/iterator_range.h>
 
-class directory_range
+namespace zelda { 
+
+inline iterator_range<boost::filesystem::directory_iterator>
+    directory_range(const boost::filesystem::path& p)
 {
+    return make_iterator_range(boost::filesystem::directory_iterator(p),
+                               boost::filesystem::directory_iterator());
+}
 
-};
+inline iterator_range<boost::filesystem::recursive_directory_iterator>
+    recursive_directory_range(const boost::filesystem::path& p)
+{
+    return make_iterator_range(boost::filesystem::recursive_directory_iterator(p),
+                               boost::filesystem::recursive_directory_iterator());
+}
 
-}}
+}
 
 #endif
