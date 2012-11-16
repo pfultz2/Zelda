29090cb9ec04faf061ab09b37224b3a7896040c9
paul <paul@paul-TPOWER-X79>
2012-11-15 23:23:42 -0600

    Add irange

---
+++ zelda/range/irange.h
@@ -0,0 +1,19 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    irange.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_RANGE_IRANGE_H
+#define ZELDA_GUARD_RANGE_IRANGE_H
+
+#include <boost/range/irange.hpp>
+
+namespace zelda { 
+
+using boost::irange;
+
+}
+
+#endif
