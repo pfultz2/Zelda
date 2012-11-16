0a55f1af050bacf71e1e6b6080f9a470f7b210f2
paul <paul@paul-TPOWER-X79>
2012-11-15 22:52:11 -0600

    Added stream_lines

--- zelda/range/stream_lines.h
+++ zelda/range/stream_lines.h
@@ -8,13 +8,38 @@
 #ifndef ZELDA_GUARD_RANGE_STREAM_LINES_H
 #define ZELDA_GUARD_RANGE_STREAM_LINES_H
 
-namespace zelda { namespace range {
+#include <zelda/range/generate.h>
+#include <boost/optional.hpp>
+#include <string>
 
-class stream_lines
+namespace zelda { namespace detail {
+
+struct stream_lines_gen
 {
+    template<class Stream>
+    boost::optional<std::string> operator()(Stream & s) const
+    {
+        std::string result;
+        if (std::getline(s, result)) return result;
+        else return boost::optional<std::string>();
+    }
 
+    template<class Stream>
+    boost::optional<std::string> operator()(Stream & s, char delim) const
+    {
+        std::string result;
+        if (std::getline(s, result, delim)) return result;
+        else return boost::optional<std::string>();
+    }
 };
 
-}}
+}
+// TODO: Allow custom delimiter
+ZELDA_FUNCTION_OBJECT((stream_lines)(s)
+    (
+        generate(boost::bind(detail::stream_lines_gen(), s))
+    ))
+
+}
 
 #endif
