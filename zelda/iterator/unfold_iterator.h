3f8b4b22cb56caab9c031e8caac82ccd4d1bf8c1
paul <paul@paul-TPOWER-X79>
2012-11-16 00:15:46 -0600

    Updated author and copyright

--- zelda/iterator/unfold_iterator.h
+++ zelda/iterator/unfold_iterator.h
@@ -1,5 +1,5 @@
 /*=============================================================================
-    Copyright (c) 2012 Paul Fultz II
+    Copyright (c) 2005-2007 Shunsuke Sogame
     unfold_iterator.h
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
