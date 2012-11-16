c7a90dbc284c3fa85e153d7ae94e5277c6a85321
paul <paul@paul-TPOWER-X79>
2012-11-15 23:23:21 -0600

    Added any_range

---
+++ zelda/range/any_range.h
@@ -0,0 +1,19 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    any_range.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_RANGE_ANY_RANGE_H
+#define ZELDA_GUARD_RANGE_ANY_RANGE_H
+
+#include <boost/range/any_range.hpp>
+
+namespace zelda { 
+
+using boost::range::any_range;
+
+}
+
+#endif
