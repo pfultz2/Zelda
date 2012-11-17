eccdc4df529880241aee6de13aab6e533688d4c5
paul <paul@paul-TPOWER-X79>
2012-11-17 16:53:30 -0600

    Added all_of

---
+++ zelda/algorithm/all_of.h
@@ -0,0 +1,34 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    all_of.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ALGORITHM_ALL_OF_H
+#define ZELDA_GUARD_ALGORITHM_ALL_OF_H
+
+#include <zelda/function/builder.h>
+#include <zelda/traits.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+
+#include <zelda/algorithm/find.h>
+
+#include <boost/fusion/algorithm/query/all.hpp>
+#include <algorithm>
+
+#include <boost/bind.hpp>
+#include <functional>
+
+namespace zelda { 
+
+ZELDA_FUNCTION_PIPE_OBJECT((all_of)(r, f)
+        if (is_sequence<r>)(boost::fusion::all(r, f))
+        else if (is_range<r>)(zelda::find(r, boost::bind(std::logical_not<bool>(), boost::bind(f, _1))) != boost::end(r))
+
+    )
+
+}
+
+#endif
