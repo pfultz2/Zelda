a0e5f8075823350184dd70c7276937d6768941ca
paul <paul@paul-TPOWER-X79>
2012-11-17 16:37:31 -0600

    Added find

---
+++ zelda/algorithm/find.h
@@ -0,0 +1,59 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    find.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ALGORITHM_FIND_H
+#define ZELDA_GUARD_ALGORITHM_FIND_H
+
+#include <zelda/function/builder.h>
+#include <zelda/traits.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+
+#include <algorithm>
+#include <string>
+
+namespace zelda { 
+
+namespace detail {
+
+template<class Range, class T>
+typename boost::range_iterator<Range>::type
+find(const Range& r, const T& x)
+{
+    return std::find(boost::begin(r), boost::end(r), x);
+}
+
+template<class Range, class T>
+typename boost::range_iterator<Range>::type
+adl_find(const Range& r, const T& x)
+{
+    return find(r, x);
+}
+
+template<class T>
+struct has_faux_find
+: boost::mpl::bool_<false>
+{};
+
+
+template<class C, class A, class T>
+struct has_faux_find<std::basic_string<C, A, T> >
+: boost::mpl::bool_<true>
+{};
+
+}
+
+ZELDA_FUNCTION_PIPE_OBJECT((find)(r, x)
+        if (is_sub_range<r, x>)(std::search(boost::begin(r), boost::end(r), boost::begin(x), boost::end(x)))
+        else if (detail::has_faux_find<r>)(std::search(boost::begin(r), boost::end(r), boost::begin(x), boost::end(x)))
+        else if (is_range<r>)(detail::adl_find(r, x))
+    )   
+
+
+}
+
+#endif
