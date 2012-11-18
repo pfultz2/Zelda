ba3b828b0339d699bd3a3eb32599e597031d975c
paul <paul@paul-TPOWER-X79>
2012-11-18 01:26:00 -0600

    Added replace

---
+++ zelda/algorithm/replace.h
@@ -0,0 +1,32 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    replace.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ALGORITHM_REPLACE_H
+#define ZELDA_GUARD_ALGORITHM_REPLACE_H
+
+#include <zelda/algorithm/transform.h>
+#include <boost/bind.hpp>
+
+namespace zelda { 
+
+namespace detail {
+
+ZELDA_FUNCTION_CLASS((replacer)(auto x, auto old_value, auto new_value)
+    (
+        x == old_value ? new_value : x
+    ))
+
+}
+
+ZELDA_FUNCTION_PIPE_OBJECT((replace)(auto r, auto old_value, auto new_value)
+    if (is_range<r>)(zelda::transform(r, boost::bind(detail::replacer(), _1, old_value, new_value)))
+
+    )
+
+}
+
+#endif
