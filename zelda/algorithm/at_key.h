a92c53191f07d9f6a5974f6eb3ac712f4b5e2ed7
paul <paul@paul-TPOWER-X79>
2012-11-18 00:00:47 -0600

    Added at_key

---
+++ zelda/algorithm/at_key.h
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    at_key.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ALGORITHM_AT_KEY_H
+#define ZELDA_GUARD_ALGORITHM_AT_KEY_H
+
+#include <boost/fusion/sequence/intrinsic/at_key.hpp>
+
+namespace zelda { 
+
+// TODO: Make pipable
+using boost::fusion::at_key;
+
+}
+
+#endif
