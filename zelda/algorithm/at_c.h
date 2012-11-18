976611efc102502bc2920955d695a3973ed3d370
paul <paul@paul-TPOWER-X79>
2012-11-18 00:00:07 -0600

    Added at_c

---
+++ zelda/algorithm/at_c.h
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    at_c.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_ALGORITHM_AT_C_H
+#define ZELDA_GUARD_ALGORITHM_AT_C_H
+
+#include <boost/fusion/sequence/intrinsic/at_c.hpp>
+
+namespace zelda { 
+
+// TODO: Make pipable
+using boost::fusion::at_c;
+
+}
+
+#endif
