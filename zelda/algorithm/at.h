14c6534db3db8fabf23f2907d94b35d9063ea560
Paul <pfultz2@yahoo.com>
2012-11-22 13:46:10 -0500

    Renamed

--- zelda/algorithm/at.h
+++ zelda/algorithm/at.h
@@ -25,7 +25,7 @@ namespace detail {
 template<class R, class I>
 ZELDA_FUNCTION_REQUIRES(has_range_traversal<R, boost::random_access_traversal_tag>)
 (typename boost::range_iterator<R>::type)
-range_at(R& r, I n)
+iterator_at(R& r, I n)
 {
     typename boost::range_iterator<const R>::type it;
     if (n < 0) it = boost::end(r);
@@ -38,7 +38,7 @@ range_at(R& r, I n)
 ZELDA_FUNCTION_PIPE_OBJECT((at)(r, n)
         if (has_range_traversal<r, boost::random_access_traversal_tag>)
         (
-            ZELDA_ASSERT_EXPR(!zelda::empty(r), detail::range_at(r))
+            ZELDA_ASSERT_EXPR(!zelda::empty(r), detail::iterator_at(r))
         )
 
     )
