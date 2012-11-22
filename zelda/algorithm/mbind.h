5ea56dc5a6cce2383902e73e02cf2f126d94f39e
Paul <pfultz2@yahoo.com>
2012-11-22 13:02:42 -0500

    Added mbind

--- zelda/algorithm/mbind.h
+++ zelda/algorithm/mbind.h
@@ -8,13 +8,29 @@
 #ifndef ZELDA_GUARD_ALGORITHM_MBIND_H
 #define ZELDA_GUARD_ALGORITHM_MBIND_H
 
-namespace zelda { namespace algorithm {
+#include <zelda/function/builder.h>
+#include <zelda/traits.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
 
-class mbind
-{
+#include <zelda/iterator/mbind_iterator.h>
+#include <zelda/range/iterator_range.h>
+#include <algorithm>
 
-};
+namespace zelda { 
 
-}}
+ZELDA_FUNCTION_PIPE_OBJECT((mbind)(r, f)
+    if (is_range<r>)
+    (
+        make_iterator_range
+        (
+            make_bind_iterator(r, boost::begin(r), boost::end(r)),
+            make_bind_iterator(r, boost::end(r), boost::end(r))
+        )
+    )
+
+)
+
+}
 
 #endif
