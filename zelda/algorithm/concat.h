a15114900fae4c0d3d1f7bd322213eb93c461d0b
Paul <pfultz2@yahoo.com>
2012-11-22 13:12:48 -0500

    Add concat

--- zelda/algorithm/concat.h
+++ zelda/algorithm/concat.h
@@ -8,13 +8,30 @@
 #ifndef ZELDA_GUARD_ALGORITHM_CONCAT_H
 #define ZELDA_GUARD_ALGORITHM_CONCAT_H
 
-namespace zelda { namespace algorithm {
+#include <zelda/function/builder.h>
+#include <zelda/traits.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
 
-class concat
-{
+#include <zelda/algorithm/mbind.h>
+#include <algorithm>
 
-};
+#include <boost/bind.hpp>
+#include <functional>
 
-}}
+namespace zelda { 
+
+namespace detail {
+
+ZELDA_FUNCTION_CLASS((concat_identity)(x)(x))
+
+}
+
+ZELDA_FUNCTION_PIPE_OBJECT((concat)(r)
+    if (is_range<r>)(mbind(r, detail::concat_identity()))
+
+)
+
+}
 
 #endif
