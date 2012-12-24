e7c2cd5b18131eb87e78a6bd95d894a0ec4d0711
Paul <pfultz2@yahoo.com>
2012-12-24 15:52:13 -0500

    Added uper_bound

--- zelda/algorithm/upper_bound.h
+++ zelda/algorithm/upper_bound.h
@@ -8,13 +8,24 @@
 #ifndef ZELDA_GUARD_ALGORITHM_UPPER_BOUND_H
 #define ZELDA_GUARD_ALGORITHM_UPPER_BOUND_H
 
-namespace zelda { namespace algorithm {
+#include <zelda/function/builder.h>
+#include <zelda/traits.h>
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/empty.hpp>
 
-class upper_bound
-{
+#include <algorithm>
 
-};
+namespace zelda { 
 
-}}
+ZELDA_FUNCTION_PIPE_OBJECT((upper_bound)(auto r, x)
+    if (is_range<r>)
+    (
+        std::upper_bound(boost::begin(r), boost::end(r), x)
+    )
+
+)
+
+}
 
 #endif
