9a08050d7ea6b45e4abc4cd4d9901e5c11fdc2c8
paul <paul@paul-TPOWER-X79>
2013-02-21 15:12:13 -0600

    Fixed misplaced paren in testing framework

--- zelda/test.h
+++ zelda/test.h
@@ -8,15 +8,18 @@
 #ifndef ZELDA_GUARD_ZELDA_TEST_H
 #define ZELDA_GUARD_ZELDA_TEST_H
 
+#include <zelda/equals.h>
 #include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <zelda/foreach.h>
 #include <vector>
 #include <map>
 
 #define ZELDA_TEST_CASE(name) \
 struct name \
-{ void operator()(failure_callback fail); }; \
-static zelda::test::auto_register name ## _register = auto_register(#name, name()); \
-void name::operator()(failure_callback zelda_test_fail)
+{ void operator()(zelda::test::failure_callback fail) const; }; \
+static zelda::test::auto_register name ## _register = zelda::test::auto_register(#name, name()); \
+void name::operator()(zelda::test::failure_callback zelda_test_fail) const
 
 #define ZELDA_DETAIL_TEST_FAIL(message) if (zelda_test_fail(message, __LINE__)) return
 #define ZELDA_DETAIL_TEST_FAIL_EX(message) if (zelda_test_fail(message, __LINE__)) throw
@@ -24,27 +27,21 @@ void name::operator()(failure_callback zelda_test_fail)
 #define ZELDA_DETAIL_TEST_EXEC(rethrow, ...) \
 do \
 { \
-    try { __VA_ARGS__ } \
-    catch(const std::exception& zelda_ex) { ZELDA_DETAIL_TEST_FAIL_EX(ex.what()); rethrow } \
+    try { __VA_ARGS__; } \
+    catch(const std::exception& zelda_ex) { ZELDA_DETAIL_TEST_FAIL_EX(zelda_ex.what()); rethrow } \
     catch(...) { ZELDA_DETAIL_TEST_FAIL_EX("An unknown exception has occured"); rethrow } \
 } while(0)
 
 #define ZELDA_TEST_EXEC(...) ZELDA_DETAIL_TEST_EXEC(throw;, __VA_ARGS__)
 
 #define ZELDA_TEST_EQUAL(x, ...) \
-ZELDA_TEST_EXEC(if (x != __VA_ARGS__) ZELDA_DETAIL_TEST_FAIL(std::string(#x) + " != " + #__VA_ARGS__))
+ZELDA_TEST_EXEC(if (!zelda::equals(x, __VA_ARGS__)) ZELDA_DETAIL_TEST_FAIL(std::string(#x) + " != " + #__VA_ARGS__))
 
 namespace zelda { namespace test {
 
-template<class T>
-std::string debug_string(const T & x);
-
-template<class T, class U>
-bool debug_equal(const T& x, const T& y);
-
 typedef boost::function<bool(std::string message, long line)> failure_callback;
 typedef boost::function<void(failure_callback fail)> test_case;
-static std::vector<std::pair<std::string, test_case> test_cases;
+static std::vector<std::pair<std::string, test_case> > test_cases;
 
 struct auto_register
 {
@@ -53,6 +50,27 @@ struct auto_register
         test_cases.push_back(std::make_pair(name, tc));
     }
 };
+
+struct out_failure
+{
+    typedef bool result_type;
+    bool operator()(bool & failed, std::string name, std::string message, long line) const
+    {
+        std::cout << "*****FAILED: " << name << "@" << line << std::endl << message << std::endl;
+        failed = true;
+        return true;
+    }
+};
+
+void run()
+{
+    bool failed = false;
+    ZELDA_FOREACH(const auto& tc, test_cases)
+    {
+        tc.second(boost::bind(out_failure(), boost::ref(failed), tc.first, _1, _2));
+    }
+    if (!failed) std::cout << "All " << test_cases.size() << " test cases passed." << std::endl;
+}
 
 }}
 
