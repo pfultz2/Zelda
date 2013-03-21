98b0c0d18836b835bb847b7fab783986f98c0929
paul <paul@paul-TPOWER-X79>
2013-03-20 19:24:39 -0500

    Added regular adaptor

---
+++ zelda/function/regular.h
@@ -0,0 +1,91 @@
+/*=============================================================================
+    Copyright (c) 2012 Paul Fultz II
+    regular.h
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+==============================================================================*/
+
+#ifndef ZELDA_GUARD_FUNCTION_REGULAR_H
+#define ZELDA_GUARD_FUNCTION_REGULAR_H
+
+#include <zelda/function/adaptor.h>
+#include <zelda/function/perfect.h>
+#include <zelda/function/variadic.h>
+#include <zelda/function/invoke.h>
+
+namespace zelda { namespace detail {
+
+
+template<class F>
+struct regular_base
+{
+    boost::optional<F> f;
+
+    typedef F function_type;
+
+    regular_base()
+    {}
+    template<class X>
+    regular_base(X f): f(f)
+    {}
+
+    regular_base(const regular_base & rhs) : f(rhs.f)
+    {}
+
+    // Assignment operator is just a copy construction, which does not provide
+    // the strong exception guarentee.
+    regular_base& operator=(const regular_base& rhs)
+    {
+        if (this != &rhs)
+        {
+            this->~regular_base();
+            new (this) regular_base(rhs);
+        }
+        return *this;
+    }
+
+    template<class X, class Enable = void>
+    struct result;
+
+    template<class X, class T>
+    struct result<X(T), ZELDA_CLASS_REQUIRES(boost::fusion::traits::is_sequence<typename boost::decay<T>::type>)>
+    : invoke_result<F, const typename boost::decay<T>::type&> 
+    {}; 
+
+    template<class T>
+    typename result<F(const T&)>::type operator()(const T & x) const
+    {
+        return invoke(this->f, x);
+    }
+};
+}
+
+template<class F>
+struct regular_adaptor : zelda::variadic_adaptor<detail::regular_base<F> >
+{
+    regular_adaptor()
+    {}
+
+    template<class X>
+    regular_adaptor(X x) : zelda::variadic_adaptor<detail::regular_base<F> >(x)
+    {}
+};
+
+template<class F>
+regular_adaptor<F> regular(F f)
+{
+    return regular_adaptor<F>(f);
+}
+
+}
+
+#ifdef ZELDA_TEST
+#include <zelda/test.h>
+
+// ZELDA_TEST_CASE(regular_test)
+// {
+// }
+
+#endif
+
+#endif
