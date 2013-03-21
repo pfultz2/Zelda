cd1bfc9e19316daa027c12ddc979880a4e57f905
paul <paul@paul-TPOWER-X79>
2013-03-21 16:20:44 -0500

    Improve is_callabe to work on function pointers

--- zelda/function/is_callable.h
+++ zelda/function/is_callable.h
@@ -14,6 +14,14 @@
 #include <zelda/forward.h>
 #include <zelda/static_assert.h>
 #include <boost/mpl/bool.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/function_types/is_function.hpp>
+#include <boost/function_types/is_function_pointer.hpp>
+
+#ifndef ZELDA_IS_CALLABLE_LIMIT
+#define ZELDA_IS_CALLABLE_LIMIT 16
+#endif
 
 namespace zelda {
 
@@ -57,51 +65,63 @@ struct funwrap<n, Fun> : Fun \
 BOOST_PP_REPEAT_1(16, ZELDA_FUNWRAP_BUILDER, ~)
 #undef ZELDA_FUNWRAP_BUILDER
 
-// template<class Fun> struct funwrap<0, Fun> : Fun
-// {
-//     funwrap();
-//     typedef private_type const &(*pointer_to_function)();
-//     operator pointer_to_function() const;
-// };
-// template<class Fun> struct funwrap<1, Fun> : Fun
-// {
-//     funwrap();
-//     typedef private_type const &(*pointer_to_function)( dont_care );
-//     operator pointer_to_function() const;
-// };
+template<class F, class G>
+struct is_callable_fp
+: boost::mpl::bool_<false>
+{};
+#define ZELDA_IS_CALLABLE_FP(z, n, data) \
+template<class Fun BOOST_PP_COMMA_IF(n) ZELDA_PP_PARAMS_Z(z, n, class T), \
+         class Res BOOST_PP_COMMA_IF(n) ZELDA_PP_PARAMS_Z(z, n, class U)> \
+struct is_callable_fp<Fun(ZELDA_PP_PARAMS_Z(z, n, T)), Res(*)(ZELDA_PP_PARAMS_Z(z, n, U))> \
+{ \
+    static bool const value = \
+        (true ZELDA_PP_GEN_Z(z, n, && boost::is_convertible<T, ZELDA_PP_INTERCEPT_COMMA, U, >::value BOOST_PP_INTERCEPT)); \
+    typedef boost::mpl::bool_<value> type; \
+};
+BOOST_PP_REPEAT_1(ZELDA_IS_CALLABLE_LIMIT, ZELDA_IS_CALLABLE_FP, ~)
 
-
+template<class F>
+struct is_callable_fo;
+#define ZELDA_IS_CALLABLE_FO(z, n, data) \
+template<class Fun BOOST_PP_COMMA_IF(n) ZELDA_PP_PARAMS_Z(z, n, class T)> \
+struct is_callable_fo<Fun(ZELDA_PP_PARAMS_Z(z, n, T))> \
+{ \
+    static funwrap<n, Fun> & fun; \
+    ZELDA_PP_GEN_Z(z, n, static typename add_forward_reference<T, >::type BOOST_PP_INTERCEPT, x, ; BOOST_PP_INTERCEPT) \
+    static bool const value = \
+    (\
+        sizeof(no_type) == sizeof(is_private_type( (fun(ZELDA_PP_PARAMS_Z(z, n, x)), 0) )) \
+    ); \
+    typedef boost::mpl::bool_<value> type; \
+};
+BOOST_PP_REPEAT_1(ZELDA_IS_CALLABLE_LIMIT, ZELDA_IS_CALLABLE_FO, ~)
 
 }
 
 #define ZELDA_IS_CALLABLE_BUILDER(z, n, data) \
 template<class Fun BOOST_PP_COMMA_IF(n) ZELDA_PP_PARAMS_Z(z, n, class T)> \
 struct is_callable<Fun(ZELDA_PP_PARAMS_Z(z, n, T))> \
-{ \
-    static callable_detail::funwrap<n, Fun> & fun; \
-    ZELDA_PP_GEN_Z(z, n, static typename add_forward_reference<T, >::type BOOST_PP_INTERCEPT, x, ; BOOST_PP_INTERCEPT) \
-    static bool const value = \
-    (\
-        sizeof(callable_detail::no_type) == sizeof(callable_detail::is_private_type( (fun(ZELDA_PP_PARAMS_Z(z, n, x)), 0) )) \
-    ); \
-    typedef boost::mpl::bool_<value> type; \
-};
-BOOST_PP_REPEAT_1(16, ZELDA_IS_CALLABLE_BUILDER, ~)
-#undef ZELDA_IS_CALLABLE_BUILDER
+: boost::mpl::eval_if<boost::function_types::is_function_pointer<Fun>, \
+                    callable_detail::is_callable_fp<Fun(ZELDA_PP_PARAMS_Z(z, n, T)), Fun>, \
+                    callable_detail::is_callable_fo<Fun(ZELDA_PP_PARAMS_Z(z, n, T))> >::type \
+{};
+BOOST_PP_REPEAT_1(ZELDA_IS_CALLABLE_LIMIT, ZELDA_IS_CALLABLE_BUILDER, ~)
 
-// template<class Fun > struct is_callable<Fun()>
-// {
-//     static callable_detail::funwrap<0, Fun> & fun;
-//     static bool const value = ( sizeof(callable_detail::no_type) == sizeof(callable_detail::is_private_type( (fun(), 0) )) );
-//     typedef boost::mpl::bool_<value> type;
+// #define ZELDA_IS_CALLABLE_BUILDER(z, n, data) \
+// template<class Fun BOOST_PP_COMMA_IF(n) ZELDA_PP_PARAMS_Z(z, n, class T)> \
+// struct is_callable<Fun(ZELDA_PP_PARAMS_Z(z, n, T))> \
+// { \
+//     static callable_detail::funwrap<n, Fun> & fun; \
+//     ZELDA_PP_GEN_Z(z, n, static typename add_forward_reference<T, >::type BOOST_PP_INTERCEPT, x, ; BOOST_PP_INTERCEPT) \
+//     static bool const value = \
+//     (\
+//         sizeof(callable_detail::no_type) == sizeof(callable_detail::is_private_type( (fun(ZELDA_PP_PARAMS_Z(z, n, x)), 0) )) \
+//     ); \
+//     typedef boost::mpl::bool_<value> type; \
 // };
-// template<class Fun , class T0 > struct is_callable<Fun( T0 )>
-// {
-//     static callable_detail::funwrap<1, Fun> & fun;
-//     static T0 x0 ;
-//     static bool const value = ( sizeof(callable_detail::no_type) == sizeof(callable_detail::is_private_type( (fun( x0 ), 0) )) );
-//     typedef boost::mpl::bool_<value> type;
-// };
+// BOOST_PP_REPEAT_1(ZELDA_IS_CALLABLE_LIMIT, ZELDA_IS_CALLABLE_BUILDER, ~)
+// #undef ZELDA_IS_CALLABLE_BUILDER
+
 
 #else
 
@@ -135,12 +155,26 @@ struct is_callable_class
 };
 struct callable_test_param {};
 
-typedef is_callable<is_callable_class(int)>::type is_callable_test;
-static_assert(is_callable_test::value, "Not callable");
+void is_callable_function(int)
+{
+}
+
+// typedef is_callable<is_callable_class(int)>::type is_callable_test;
+static_assert(is_callable<is_callable_class(int)>::type::value, "Not callable");
 static_assert(is_callable<is_callable_class(const int&)>::type::value, "Not callable");
 static_assert(not is_callable<is_callable_class(callable_test_param)>::type::value, "callable failed");
 static_assert(not is_callable<is_callable_class()>::type::value, "callable failed");
 static_assert(not is_callable<is_callable_class(int, int)>::type::value, "callable failed");
+
+typedef void (*is_callable_function_pointer)(int);
+static_assert(boost::function_types::is_function_pointer<is_callable_function_pointer>::value, "Not callable");
+// typedef boost::mpl::identity<is_callable_function_pointer>::type is_callable_function_type;
+// typedef is_callable<is_callable_function_pointer(int)>::type is_callable_test;
+static_assert(is_callable<is_callable_function_pointer(int)>::type::value, "Not callable");
+static_assert(is_callable<is_callable_function_pointer(const int&)>::type::value, "Not callable");
+static_assert(not is_callable<is_callable_function_pointer(callable_test_param)>::type::value, "callable failed");
+static_assert(not is_callable<is_callable_function_pointer()>::type::value, "callable failed");
+static_assert(not is_callable<is_callable_function_pointer(int, int)>::type::value, "callable failed");
 
 #endif
 
