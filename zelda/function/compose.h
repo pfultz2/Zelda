a5016fc05281e9b496855642ad5c987b624fb06d
Paul <paul@ned>
2013-04-19 23:16:23 -0500

    Add documentation for compose

--- zelda/function/compose.h
+++ zelda/function/compose.h
@@ -8,6 +8,56 @@
 #ifndef ZELDA_GUARD_FUNCTION_COMPOSE_H
 #define ZELDA_GUARD_FUNCTION_COMPOSE_H
 
+// @begin
+// compose
+// ======
+// 
+// Description
+// -----------
+// 
+// The `compose` function adaptor provides funcion composition. It produces a
+// function object that composes a set of functions, ie the output of one
+// function becomes the input of the second function. So, `compose(f, g)(0)` is
+// equivalent to `g(f(0))`.
+// 
+// Note: Functions used in the `compose_adaptor` are evaluated in the order they
+// are given, rather than order they are written, which is way it represented
+// mathematically. For example, mathematically, `g(f(x))` is written `g * f`,
+// however, using the adaptor it is written `compose(f, g)` since `f` is
+// evaluated first.
+// 
+// Synopsis
+// --------
+// 
+//     template<class F1, class F2, ...>
+//     compose_adaptor<F1, F2, ...> compose(F1 f1, F2 f2, ...);
+// 
+// Example
+// -------
+// 
+//     struct increment
+//     {
+//         template<class T>
+//         T operator()(T x) const
+//         {
+//             return x + 1;
+//         }
+//     };
+// 
+//     struct decrement
+//     {
+//         template<class T>
+//         T operator()(T x) const
+//         {
+//             return x - 1;
+//         }
+//     };
+// 
+//     int r = compose(increment(), decrement(), increment())(3);
+//     assert(r == 4);
+// 
+// @end
+
 #include <zelda/function/variadic.h>
 #include <zelda/function/fuse.h>
 #include <boost/fusion/algorithm/iteration/fold.hpp>
