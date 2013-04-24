479a8bc0054a902a671a09c8253f38c2ad38072c
Paul <pfultz2@yahoo.com>
2013-04-23 22:56:35 -0400

    Add documentation for implicit

--- zelda/function/implicit.h
+++ zelda/function/implicit.h
@@ -8,6 +8,59 @@
 #ifndef ZELDA_GUARD_FUNCTION_IMPLICIT_H
 #define ZELDA_GUARD_FUNCTION_IMPLICIT_H
 
+// @begin
+// implicit
+// ========
+// 
+// Description
+// -----------
+// 
+// The `implicit_adaptor` is a static function adaptor that uses the type that
+// return value can be converted to in order to determine the type of the
+// template parameter. Since it is static function adaptor, the function must be
+// default constructible.
+// 
+// Synopsis
+// --------
+// 
+//     template<class F>
+//     class implicit_adaptor<F>;
+// 
+// Example
+// -------
+// 
+//     template<class T>
+//     struct auto_caster
+//     {
+//         template<class X>
+//         struct result
+//         {
+//             typedef T type;
+//         };
+//         template<class U>
+//         T operator()(U x)
+//         {
+//             return T(x);
+//         }
+//     };
+// 
+//     implicit<auto_caster> auto_cast = {};
+// 
+//     struct auto_caster_foo
+//     {
+//         int i;
+//         explicit auto_caster_foo(int i) : i(i) {}
+// 
+//     };
+// 
+//     float f = 1.5;
+//     int i = auto_cast(f);
+//     auto_caster_foo x = auto_cast(1);
+//     assert(1 == i);
+//     assert(1 == x.i);
+// 
+// @end
+
 #include <zelda/function/variadic.h>
 #include <zelda/function/invoke.h>
 #include <zelda/function/static.h>
