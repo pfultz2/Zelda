38f266085427e2f80ca162663aaa9d3017d2e124
Paul <pfultz2@yahoo.com>
2013-04-25 12:38:59 -0400

    Add documentation for is_callable

--- zelda/function/is_callable.h
+++ zelda/function/is_callable.h
@@ -8,6 +8,40 @@
 #ifndef ZELDA_GUARD_IS_CALLABLE_H
 #define ZELDA_GUARD_IS_CALLABLE_H
 
+// @begin
+// is_callable
+// ===========
+// 
+// Description
+// -----------
+// 
+// The `is_callable` metafunction checks if the function object is callable with
+// the certain parameters. It uses the same signature as `result_of`.
+// 
+// Note: On older compilers, sometimes `is_callable` produces a compile error due
+// to using incomplete types. To workaround this, callability can be detected by
+// using the nested `result` if its made to be SFINAE-friendly. Just add a nested
+// `zelda_is_callable_by_result_tag` to the function object.
+// 
+// Synopsis
+// --------
+// 
+//     template<class Sig>
+//     struct is_callable;
+// 
+// Example
+// -------
+// 
+//     struct is_callable_class
+//     {
+//         void operator()(int) const
+//         {
+//         }
+//     };
+//     static_assert(is_callable<is_callable_class(int)>::type::value, "Not callable");
+// 
+// @end
+
 #include <zelda/config.h>
 #include <zelda/pp.h>
 #include <zelda/typeof.h>
