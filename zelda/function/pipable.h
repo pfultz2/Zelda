3c0ae66622b81625cc3c31ebacade1aeca6e9bee
Paul <pfultz2@yahoo.com>
2013-04-26 23:36:33 -0400

    Update documentation for pipable

--- zelda/function/pipable.h
+++ zelda/function/pipable.h
@@ -8,6 +8,42 @@
 #ifndef ZELDA_GUARD_FUNCTION_PIPABLE_H
 #define ZELDA_GUARD_FUNCTION_PIPABLE_H
 
+// @begin
+// pipable
+// =======
+// 
+// Description
+// -----------
+// 
+// The `pipable` function adaptor provides an extension method. The first
+// parameter of the function can be piped into the function using the `|`
+// operator. This can be especially convient when there are a lot of nested
+// function calls. Functions that are made pipable can still be called the
+// traditional way without piping in the first parameter.
+// 
+// Synopsis
+// --------
+// 
+//     template<class F>
+//     pipable_adaptor<F> pipable(F f);
+// 
+// Example
+// -------
+// 
+//     struct sum
+//     {
+//         template<class T>
+//         T sum(T x, T y) const
+//         {
+//             return x+y;
+//         }
+//     };
+// 
+//     assert(3 == 1 | pipable(sum())(2));
+//     assert(3 == pipable(sum())(1, 2));
+// 
+// @end
+
 #include <zelda/function/adaptor.h>
 #include <zelda/function/conditional.h>
 #include <zelda/function/static.h>
